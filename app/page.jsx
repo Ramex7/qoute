89733cb00a9d327fd6c6b95768100414337fd514
Ramex7 <ramadanjamal27@gmail.com>
2023-10-15 20:28:10 +0300

    first commit

---
+++ app/page.jsx
@@ -0,0 +1,16 @@
+import Feed from '@components/Feed'
+
+const Home = () => (
+  <section className='w-full flex-center flex-col'>
+    <h1 className='head_text text-center'>Create and share your</h1>
+    <h1 className='head_text text-center'>qoute of the day</h1>
+    <p className='desc text-center'>
+      QOT is an open-source platform to share what your feeling today with just
+      50 letters
+    </p>
+
+    <Feed />
+  </section>
+)
+
+export default Home
